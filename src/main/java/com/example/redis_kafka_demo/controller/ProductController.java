7684e5db044dce422fdd2ce938fb98b326ad7686
Ростислав <icefox17@gmail.com>
2025-04-14 17:41:19 +0300

    Добавил отправку сообщения в кафку + application.yml

--- src/main/java/com/example/redis_kafka_demo/controller/ProductController.java
+++ src/main/java/com/example/redis_kafka_demo/controller/ProductController.java
@@ -3,8 +3,10 @@ package com.example.redis_kafka_demo.controller;
 import com.example.redis_kafka_demo.model.entity.Product;
 import com.example.redis_kafka_demo.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,9 +14,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
-
     @Autowired
     private ProductService productService;
+
+    @Autowired
+    private KafkaTemplate<String, Product> kafkaTemplate;
+
+    @Value("${kafka.topics.product}")
+    private static String TOPIC;
 
     @GetMapping
     public List<Product> getAllProducts() {
@@ -31,6 +38,7 @@ public class ProductController {
     @PostMapping
     public ResponseEntity<Void> createProduct(@RequestBody Product product) {
         productService.saveProduct(product);
+        kafkaTemplate.send(TOPIC, product);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
