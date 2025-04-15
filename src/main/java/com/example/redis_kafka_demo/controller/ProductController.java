f47bff10befdd9d38b784858269aef76e53b187f
Ростислав <icefox17@gmail.com>
2025-04-15 10:09:50 +0300

    рефакторинг

--- src/main/java/com/example/redis_kafka_demo/controller/ProductController.java
+++ src/main/java/com/example/redis_kafka_demo/controller/ProductController.java
@@ -3,15 +3,13 @@ package com.example.redis_kafka_demo.controller;
 import com.example.redis_kafka_demo.model.entity.Product;
 import com.example.redis_kafka_demo.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+
 
 @RestController
 @RequestMapping("/api/products")
@@ -19,11 +17,6 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
-    @Autowired
-    private KafkaTemplate<String, Product> kafkaTemplate;
-
-    @Value("${kafka.topics.product.created}")
-    private static String ADDED_PRODUCTS_TOPIC;
 
     @GetMapping
     public Page<Product> getAllProducts(Pageable pageable) {
@@ -40,7 +33,6 @@ public class ProductController {
     @PostMapping
     public ResponseEntity<Void> createProduct(@RequestBody Product product) {
         productService.saveProduct(product);
-        kafkaTemplate.send(ADDED_PRODUCTS_TOPIC, product);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
--- src/main/java/com/example/redis_kafka_demo/service/impl/ProductServiceImpl.java
+++ src/main/java/com/example/redis_kafka_demo/service/impl/ProductServiceImpl.java
@@ -4,8 +4,10 @@ import com.example.redis_kafka_demo.model.entity.Product;
 import com.example.redis_kafka_demo.repository.ProductRepository;
 import com.example.redis_kafka_demo.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,6 +18,12 @@ public class ProductServiceImpl implements ProductService {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private KafkaTemplate<String, Product> kafkaTemplate;
+
+    @Value("${kafka.topics.product.created}")
+    private static String ADDED_PRODUCTS_TOPIC;
+
     public Page<Product> getAllProducts(Pageable pageable) {
         return productRepository.findAll(pageable);
     }
@@ -25,6 +33,7 @@ public class ProductServiceImpl implements ProductService {
     }
 
     public void saveProduct(Product product) {
+        kafkaTemplate.send(ADDED_PRODUCTS_TOPIC, product);
         productRepository.save(product);
     }
 }
