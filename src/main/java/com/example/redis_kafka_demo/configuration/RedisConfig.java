df743fa8f4ae4dabbae2a8e9b827697069c2139a
r.shibaev <r.shibaev@mail.astondevs.ru>
2025-04-16 14:47:57 +0300

    поправлен редис конфиг, для корректной работы с датами

--- src/main/java/com/example/redis_kafka_demo/configuration/RedisConfig.java
+++ src/main/java/com/example/redis_kafka_demo/configuration/RedisConfig.java
@@ -1,11 +1,14 @@
 package com.example.redis_kafka_demo.configuration;
 
 import com.example.redis_kafka_demo.model.entity.Product;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -15,12 +18,15 @@ public class RedisConfig {
     public RedisTemplate<String, Product> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Product> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
-
-        // Ключи как строки
         template.setKeySerializer(new StringRedisSerializer());
 
-        // Значения — с GenericJackson2JsonRedisSerializer
-        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer();
+        // Создаем кастомный ObjectMapper
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        // Используем новый конструктор
+        Jackson2JsonRedisSerializer<Product> serializer = new Jackson2JsonRedisSerializer<>(objectMapper, Product.class);
 
         template.setValueSerializer(serializer);
         template.setHashKeySerializer(new StringRedisSerializer());
@@ -28,4 +34,5 @@ public class RedisConfig {
 
         return template;
     }
+
 }
