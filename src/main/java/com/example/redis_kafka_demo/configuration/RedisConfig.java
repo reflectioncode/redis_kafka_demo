16acccf721ae548bcacbf4c5f7bd93103fb6cea3
r.shibaev <r.shibaev@mail.astondevs.ru>
2025-04-16 14:58:42 +0300

    objectMapper вынесен вмконфиг

--- src/main/java/com/example/redis_kafka_demo/configuration/RedisConfig.java
+++ src/main/java/com/example/redis_kafka_demo/configuration/RedisConfig.java
@@ -2,8 +2,6 @@ package com.example.redis_kafka_demo.configuration;
 
 import com.example.redis_kafka_demo.model.entity.Product;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -15,17 +13,14 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
 
     @Bean
-    public RedisTemplate<String, Product> redisTemplate(RedisConnectionFactory connectionFactory) {
+    public RedisTemplate<String, Product> redisTemplate(
+            RedisConnectionFactory connectionFactory,
+            ObjectMapper objectMapper // <-- внедряем бин
+    ) {
         RedisTemplate<String, Product> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
 
-        // Создаем кастомный ObjectMapper
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-
-        // Используем новый конструктор
         Jackson2JsonRedisSerializer<Product> serializer = new Jackson2JsonRedisSerializer<>(objectMapper, Product.class);
 
         template.setValueSerializer(serializer);
@@ -34,5 +29,4 @@ public class RedisConfig {
 
         return template;
     }
-
 }
